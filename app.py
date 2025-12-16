3ac88ebb99b1ca99bb7309b11d6c150524e0f7cb
integraleacademy <clement.annecy@gmail.com>
2025-12-16 06:25:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ USERS = {
 }
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path="/static")
 app.secret_key = "cle-flask-secrete-2024"
 
 from datetime import date
