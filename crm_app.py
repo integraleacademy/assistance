83b0f5b0c6b338d7878b0d7357bb6e54a8bb3193
integraleacademy <clement.annecy@gmail.com>
2026-08-03 14:35:52 +0200

    fix: brancher le CRM sur le suivi CNAPS Gestion stagiaires

--- crm_app.py
+++ crm_app.py
@@ -1,6 +1,16 @@
 """Point d'entrée Gunicorn enrichi des extensions CRM isolées."""
 
-from app import app
+import app as legacy_app
+
+from crm_cnaps_tracking import register_cnaps_tracking_proxy
 from crm_salesforce_import import register_salesforce_import
 
+
+app = legacy_app.app
 register_salesforce_import(app)
+register_cnaps_tracking_proxy(
+    app,
+    load_data=legacy_app.load_data,
+    find_contact=legacy_app._crm_contact,
+    login_required=legacy_app.login_required,
+)
