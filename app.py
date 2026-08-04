1be4cbb81131de05cf70433f0813167a02ea9093
integraleacademy <clement.annecy@gmail.com>
2026-08-04 14:39:55 +0200

    Rétablit l’accès aux pages CRM

--- app.py
+++ app.py
@@ -7342,6 +7342,14 @@ def crm(section):
         section=section,
         statuses=_crm_statuses(load_data()),
         user=user,
+        crm_team=[
+            {
+                "name": member["name"],
+                "first_name": member["first_name"],
+                "email": member["email"],
+            }
+            for member in USERS.values()
+        ],
         asset_version=CRM_ASSET_VERSION,
     )
 
