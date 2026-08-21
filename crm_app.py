7819a02fb486498485205d83d659e6aae7cfc2b6
integraleacademy <clement.annecy@gmail.com>
2026-08-21 05:50:04 +0200

    Enregistrer la route de migration Salesforce complète

--- crm_app.py
+++ crm_app.py
@@ -4,12 +4,20 @@ import app as legacy_app
 
 from crm_cnaps_tracking import register_cnaps_tracking_proxy
 from crm_salesforce_import import register_salesforce_import
+from crm_salesforce_migration import register_salesforce_migration
 from secretariat_followup_patch import register_secretariat_followup_patch
 
 
 app = legacy_app.app
 register_secretariat_followup_patch(legacy_app)
 register_salesforce_import(app)
+register_salesforce_migration(
+    app,
+    current_user_fn=legacy_app.current_user,
+    load_data_fn=legacy_app.load_data,
+    login_required_fn=legacy_app.login_required,
+    save_data_fn=legacy_app.save_data,
+)
 register_cnaps_tracking_proxy(
     app,
     load_data=legacy_app.load_data,
