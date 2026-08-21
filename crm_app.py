cfacd8a24e55551b64ae05ffaf235c102457db3c
integraleacademy <clement.annecy@gmail.com>
2026-08-21 06:12:59 +0200

    feat(crm): register Google Ads conversion integration

--- crm_app.py
+++ crm_app.py
@@ -3,6 +3,7 @@
 import app as legacy_app
 
 from crm_cnaps_tracking import register_cnaps_tracking_proxy
+from crm_google_ads import register_google_ads_offline_conversions
 from crm_salesforce_import import register_salesforce_import
 from secretariat_followup_patch import register_secretariat_followup_patch
 
@@ -16,3 +17,4 @@ register_cnaps_tracking_proxy(
     find_contact=legacy_app._crm_contact,
     login_required=legacy_app.login_required,
 )
+register_google_ads_offline_conversions(legacy_app)
