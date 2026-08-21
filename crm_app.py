fbac427af2001abaadada80bfa0ee651a950b5e3
integraleacademy <clement.annecy@gmail.com>
2026-08-21 05:58:20 +0200

    Activer les garde-fous de migration Salesforce

--- crm_app.py
+++ crm_app.py
@@ -1,17 +1,19 @@
 """Point d'entrée Gunicorn enrichi des extensions CRM isolées."""
 
 import app as legacy_app
+import crm_salesforce_migration as salesforce_migration
 
 from crm_cnaps_tracking import register_cnaps_tracking_proxy
 from crm_salesforce_import import register_salesforce_import
-from crm_salesforce_migration import register_salesforce_migration
+from crm_salesforce_migration_guardrails import install_salesforce_migration_guardrails
 from secretariat_followup_patch import register_secretariat_followup_patch
 
 
 app = legacy_app.app
 register_secretariat_followup_patch(legacy_app)
 register_salesforce_import(app)
-register_salesforce_migration(
+install_salesforce_migration_guardrails(salesforce_migration)
+salesforce_migration.register_salesforce_migration(
     app,
     current_user_fn=legacy_app.current_user,
     load_data_fn=legacy_app.load_data,
