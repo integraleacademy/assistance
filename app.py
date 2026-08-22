c925a1caefeec107119caf07c5416f138fbc5672
integraleacademy <clement.annecy@gmail.com>
2026-08-22 18:47:33 +0200

    chore(crm): invalider le cache des relances

--- app.py
+++ app.py
@@ -7142,7 +7142,7 @@ CRM_FT_STATUS_BY_SECONDARY = {
     for funding_status, secondary in CRM_FT_SECONDARY_BY_STATUS.items()
 }
 CRM_MANUAL_STATUS_SOURCE = "manual"
-CRM_ASSET_VERSION = "20260822-development-support-1"
+CRM_ASSET_VERSION = "20260822-overdue-reminders-1"
 
 
 def _crm_statuses(data=None):
